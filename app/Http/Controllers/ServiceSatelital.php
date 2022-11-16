cc8bcdf527ab763d10339cb96bc78158cbd9e783
Pachimanok <priopelliza@gmail.com>
2022-11-16 19:27:53 -0300

    satelital con credenciales produccion

--- app/Http/Controllers/ServiceSatelital.php
+++ app/Http/Controllers/ServiceSatelital.php
@@ -30,18 +30,18 @@ class ServiceSatelital extends Controller
             ->join('customer_load_place', 'customer_load_place.description', '=', 'carga.load_place')
             ->join('customer_unload_place', 'customer_unload_place.description', '=', 'carga.unload_place')
             ->select('cntr.id_cntr as IdTrip', 'carga.id as idCarga', 'trucks.id', 'trucks.id_satelital', 'trucks.domain', 'customer_load_place.description as LugarCarga', 'customer_load_place.lat as CargaLat', 'customer_load_place.lon as CargaLng', 'aduanas.description as LugarAduana', 'aduanas.lat as aduanaLat', 'aduanas.lon as aduanaLon', 'customer_unload_place.description as lugarDescarga', 'customer_unload_place.lat as descargaLat', 'customer_unload_place.lon as descargaLon')
-            ->where('trucks.domain', '=', 'AE792WJ')
+            /*   ->where('trucks.domain', '=', 'AE792WJ') */
             ->get();
 
-            $chek = new pruebasModel();
-                $chek->contenido = 'Consulto las patentes del Camion';
-                $chek->save();
+        $chek = new pruebasModel();
+        $chek->contenido = 'Consulto las patentes del Camion';
+        $chek->save();
 
 
         foreach ($todosMisCamiones as $camion) {
 
             $chek = new pruebasModel();
-            $chek->contenido = 'Inbgrso al Camion '. $camion->domain;
+            $chek->contenido = 'Ingreso al Camion ' . $camion->domain;
             $chek->save();
 
 
@@ -49,95 +49,122 @@ class ServiceSatelital extends Controller
             $headers = [
                 'Content-Type' => 'application/json'
             ];
+
+            // TEST: E6HW19 - PRODUCCION: C2QC20
             $body = '{
                     "patentes":["' . $camion->domain . '"],
                     "cercania":true,
                     "domicilio":false,
-                    "apiKey":"a4f0a4e8e5d34e5b7b7bc16dab941060a5c848c9",
+                    "apiCode":"C2QC20",
                     "phone":"2612128105"
                     }';
-
-            $request = new Psr7Request('GET', 'https://app.akercontrol.com/ws/v2/servicios', $headers, $body); // TEST: E6HW19 - PRODUCCION: C2QC20
+            $request = new Psr7Request('GET', 'https://app.akercontrol.com/ws/v2/servicios', $headers, $body);
             $res = $client->sendAsync($request)->wait();
             $respuesta = $res->getBody();
             $r = json_decode($respuesta, true);
             $keys = array($r);
 
-            $datos = $keys[0]['data'][$camion->domain];
-
-            $posicionLat = $datos['ult_latitud'];
-            $posicionLon = $datos['ult_longitud'];
-
-
-            $IdTrip = $camion->IdTrip;
-            return $IdTrip;
-            $Radio = 6371e3; // metres
-            $φ1 = $posicionLat * pi() / 180; // φ, λ in radians
-            $φ2 = $camion->CargaLat * pi() / 180;
-            $φ3 = $camion->aduanaLat * pi() / 180;
-            $φ4 = $camion->descargaLat * pi() / 180;
-
-            $Δφ = ($posicionLat - $camion->CargaLat) * pi() / 180;
-            $Δφ2 = ($posicionLat - $camion->aduanaLat) * pi() / 180;
-            $Δφ3 = ($posicionLat - $camion->descargaLat) * pi() / 180;
-
-            $Δλ = ($posicionLon - $camion->CargaLng) * pi() / 180;
-            $Δλ2 = ($posicionLon - $camion->aduanaLon) * pi() / 180;
-            $Δλ3 = ($posicionLon - $camion->descargaLon) * pi() / 180;
-
-            $a = sin($Δφ / 2) * sin($Δφ / 2) + cos($φ1) * cos($φ2) * sin($Δλ / 2) * sin($Δλ / 2);
-            $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-            $d = $Radio * $c; // in metres
-
-            $a2 = sin($Δφ2 / 2) * sin($Δφ2 / 2) + cos($φ1) * cos($φ3) * sin($Δλ2 / 2) * sin($Δλ2 / 2);
-            $c2 = 2 * atan2(sqrt($a2), sqrt(1 - $a2));
-            $d2 = $Radio * $c2; // in metres
-
-            $a3 = sin($Δφ3 / 2) * sin($Δφ3 / 2) + cos($φ1) * cos($φ4) * sin($Δλ3 / 2) * sin($Δλ3 / 2);
-            $c3 = 2 * atan2(sqrt($a3), sqrt(1 - $a3));
-            $d3 = $Radio * $c3; // in metres */
-
-            if ($d <= 200) { // lugar de Carga
+            if (array_key_exists('data', $r)) {
 
                 $chek = new pruebasModel();
-                $chek->contenido = 'entro a lugar de carga / Camion: '.$camion->domain;
+                $chek->contenido = 'Ingreso a a la Prueba de DATA' . $camion->domain;
                 $chek->save();
 
-                $clientCarga = new Client();
-                $requestCarga = new Psr7Request('GET', 'https://rail.com.ar/api/accionLugarDeCarga/' . $IdTrip);
-                $resCarga = $clientCarga->sendAsync($requestCarga)->wait();
-                return $resCarga;
-            }
+                $datos = $keys[0]['data'][$camion->domain];
 
-            if ($d2 <= 200) { // lugar de aduana
+                $posicionLat = $datos['ult_latitud'];
+                $posicionLon = $datos['ult_longitud'];
 
 
                 $chek = new pruebasModel();
-                $chek->contenido = 'entro a lugar de Aduana / Camion: '.$camion->domain;
+                $chek->contenido = $posicionLat . 'lon: ' . $posicionLon;
                 $chek->save();
 
-                $clientAduana = new Client();
-                $requestAduana = new Psr7Request('GET', 'https://rail.com.ar/api/accionLugarAduana/' . $IdTrip);
-                $resAduana = $clientAduana->sendAsync($requestAduana)->wait();
-                return $resAduana;
-            }
-            if ($d3 <= 200) { // lugar de descarga
 
+                $IdTrip = $camion->IdTrip;
+                $chek = new pruebasModel();
+                $chek->contenido = 'IDTrip: ' . $IdTrip;
+                $chek->save();
 
+                $Radio = 6371e3; // metres
+                $φ1 = $posicionLat * pi() / 180; // φ, λ in radians
+                $φ2 = $camion->CargaLat * pi() / 180;
+                $φ3 = $camion->aduanaLat * pi() / 180;
+                $φ4 = $camion->descargaLat * pi() / 180;
+
+                $Δφ = ($posicionLat - $camion->CargaLat) * pi() / 180;
+                $Δφ2 = ($posicionLat - $camion->aduanaLat) * pi() / 180;
+                $Δφ3 = ($posicionLat - $camion->descargaLat) * pi() / 180;
+
+                $Δλ = ($posicionLon - $camion->CargaLng) * pi() / 180;
+                $Δλ2 = ($posicionLon - $camion->aduanaLon) * pi() / 180;
+                $Δλ3 = ($posicionLon - $camion->descargaLon) * pi() / 180;
+
+                $a = sin($Δφ / 2) * sin($Δφ / 2) + cos($φ1) * cos($φ2) * sin($Δλ / 2) * sin($Δλ / 2);
+                $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+                $d = $Radio * $c; // in metres
+
+                $a2 = sin($Δφ2 / 2) * sin($Δφ2 / 2) + cos($φ1) * cos($φ3) * sin($Δλ2 / 2) * sin($Δλ2 / 2);
+                $c2 = 2 * atan2(sqrt($a2), sqrt(1 - $a2));
+                $d2 = $Radio * $c2; // in metres
+
+                $a3 = sin($Δφ3 / 2) * sin($Δφ3 / 2) + cos($φ1) * cos($φ4) * sin($Δλ3 / 2) * sin($Δλ3 / 2);
+                $c3 = 2 * atan2(sqrt($a3), sqrt(1 - $a3));
+                $d3 = $Radio * $c3; // in metres */
 
                 $chek = new pruebasModel();
-                $chek->contenido = 'entro a lugar de descarga / Camion: '.$camion->domain;
+                $chek->contenido = 'Metros de Carga ' . $d;
+                $chek->save();
+                $chek = new pruebasModel();
+                $chek->contenido = 'Metros de Aduana ' . $d2;
+                $chek->save();
+                $chek = new pruebasModel();
+                $chek->contenido = 'Metros de Descarga ' . $d3;
                 $chek->save();
 
-                $clientDescarga = new Client();
-                $requestDescarga = new Psr7Request('GET', 'https://rail.com.ar/api/accionLugarDescarga/' . $IdTrip);
-                $resDescarga = $clientDescarga->sendAsync($requestDescarga)->wait();
-                return $resDescarga;
-            }
 
-            $chek = new pruebasModel();
-            $chek->contenido = 'No esta cerca de ningun lado / camion: '.$camion->domain;
-            $chek->save();
+                if ($d <= 200) { // lugar de Carga
+
+                    $chek = new pruebasModel();
+                    $chek->contenido = 'entro a lugar de carga / Camion: ' . $camion->domain;
+                    $chek->save();
+
+                    $clientCarga = new Client();
+                    $requestCarga = new Psr7Request('GET', 'https://rail.com.ar/api/accionLugarDeCarga/' . $IdTrip);
+                    $resCarga = $clientCarga->sendAsync($requestCarga)->wait();
+                    return $resCarga;
+                }
+
+                if ($d2 <= 200) { // lugar de aduana
+
+
+                    $chek = new pruebasModel();
+                    $chek->contenido = 'entro a lugar de Aduana / Camion: ' . $camion->domain;
+                    $chek->save();
+
+                    $clientAduana = new Client();
+                    $requestAduana = new Psr7Request('GET', 'https://rail.com.ar/api/accionLugarAduana/' . $IdTrip);
+                    $resAduana = $clientAduana->sendAsync($requestAduana)->wait();
+                    return $resAduana;
+                }
+                if ($d3 <= 200) { // lugar de descarga
+
+
+
+                    $chek = new pruebasModel();
+                    $chek->contenido = 'entro a lugar de descarga / Camion: ' . $camion->domain;
+                    $chek->save();
+
+                    $clientDescarga = new Client();
+                    $requestDescarga = new Psr7Request('GET', 'https://rail.com.ar/api/accionLugarDescarga/' . $IdTrip);
+                    $resDescarga = $clientDescarga->sendAsync($requestDescarga)->wait();
+                    return $resDescarga;
+
+                    $chek = new pruebasModel();
+                    $chek->contenido = 'No esta cerca de ningun lado / camion: ' . $camion->domain;
+                    $chek->save();
+                }
+            }
         }
     }
 }
