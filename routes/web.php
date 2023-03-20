80e67c3383c07552471684d603abb11a097fd8b9
Pachimanok <priopelliza@gmail.com>
2023-03-20 15:41:13 -0300

    subimos detalle de variables a pagina principal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $varibles = DB::table('variables')->get();
+    return $varibles;
     return view('welcome');
 });
 
