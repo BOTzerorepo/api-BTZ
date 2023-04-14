c4a795ce98aad2be7556eb81bcc2293e98d4bbda
juaniolivares95 <juaniolivares95@gmail.com>
2023-04-14 13:43:25 -0300

    BOT-63

--- database/factories/PaymodeFactory.php
+++ database/factories/PaymodeFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\paymode>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PayMode>
  */
-class PaymodeFactory extends Factory
+class PayModeFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/factories/PaytimeFactory.php
+++ database/factories/PaytimeFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\paytime>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PayTime>
  */
-class PaytimeFactory extends Factory
+class PayTimeFactory extends Factory
 {
     /**
      * Define the model's default state.
