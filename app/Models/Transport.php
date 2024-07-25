1f2ba72da0eb505ec941fbdf870dd6d2e1497244
juaniolivares95 <100377381+juaniolivares95@users.noreply.github.com>
2024-07-25 13:29:36 -0300

    feat: :zap: Delete y Audit Transport
    Se implemento la eliminacion soft y la auditoria para el modelo transporte

--- app/Models/Transport.php
+++ app/Models/Transport.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
+use OwenIt\Auditing\Auditable;
 
-class Transport extends Model
+class Transport extends Model implements AuditableContract
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, Auditable;
 }
