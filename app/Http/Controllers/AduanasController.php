1ef7c6b51dcb9a25e09347151ce0654c1eef67b6
Pachimanok <51484110+Pachimanok@users.noreply.github.com>
2024-08-14 18:25:20 -0300

    modificaciones para master

---
+++ app/Http/Controllers/AduanasController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aduana;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AduanasController extends Controller
+{
+    public function index(){
+
+        $aduanas = Aduana::all();
+        return $aduanas;
+    }
+    public function show($id)
+    {
+        $aduana = DB::table('aduanas')->where('id',$id)->get();
+    
+        return $aduana;
+    }
+    public function store(Request $request)
+    {
+        $aduana = new Aduana();
+        $aduana->description = $request['description'];
+        $aduana->address = $request['address'];
+        $aduana->provincia = $request['provincia'];
+        $aduana->pais = $request['pais'];
+        $aduana->lat = $request['lat'];
+        $aduana->lon = $request['lon'];
+        $aduana->user = $request['user'];
+        $aduana->km_from_town = 0;
+        $aduana->link_maps = 'https://www.google.es/maps?q='. $request['lat'] .','. $request['lon'];
+
+        $aduana->company = $request['company'];
+        $aduana->save();
+
+        return $aduana;
+    }
+    public function update(Request $request,  $id)
+    {
+        $aduana = Aduana::findOrFail($id);
+        $aduana->description = $request['description'];
+        $aduana->address = $request['address'];
+        $aduana->provincia = $request['provincia'];
+        $aduana->pais = $request['pais'];
+        $aduana->lat = $request['lat'];
+        $aduana->lon = $request['lon'];
+        $aduana->link_maps = 'https://www.google.es/maps?q=' . $request['lat'] . ',' . $request['lon'];
+        $aduana->user = $request['user'];
+        $aduana->company = $request['company'];
+        $aduana->save();
+
+        return $aduana;
+    }
+}
