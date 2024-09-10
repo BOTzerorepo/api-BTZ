b4334864d9b33d1a6d815d24b6b45da6c4938150
Pachimanok <51484110+Pachimanok@users.noreply.github.com>
2024-09-10 16:44:52 -0300

    modificaciones para usuario transporre

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\cntrController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FleteroController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -192,9 +194,17 @@ Route::get('/user/{user}', 'App\Http\Controllers\UserController@show');
 
 Route::resource('/cntr',cntrController::class);
 
+
+// FLETERO CONTROLLER 
+
+Route::resource('fleteros', FleteroController::class);
+Route::get('/fleteroAsociados/{id}', [FleteroController::class, 'getFleteroDetails']);
+
 // TRUCK CONTROLLLER 
 Route::post('/truck', 'App\Http\Controllers\TruckController@store'); // C
 Route::get('/trucks/{customer}', 'App\Http\Controllers\TruckController@index'); // R ALL
+Route::get('/trucksTransport/{transport}', 'App\Http\Controllers\TruckController@indexTransport'); // R ALL
+
 Route::get('/truck/{truck}', 'App\Http\Controllers\TruckController@show'); // R ONE
 Route::post('/truck/{truck}', 'App\Http\Controllers\TruckController@update'); // U 
 Route::delete('/truck/{truck}', 'App\Http\Controllers\TruckController@destroy'); // D 
@@ -218,7 +228,9 @@ Route::delete('/ata/{id}','App\Http\Controllers\AtaController@destroy'); //Elimi
 // DRIVER CONTROLLLER trailerAsign
 
 Route::get('/drivers/{transport_id}','App\Http\Controllers\DriverController@showDriver'); 
-Route::get('/drivers','App\Http\Controllers\DriverController@index'); 
+Route::get('/drivers','App\Http\Controllers\DriverController@index');
+Route::get('/driversTransport/{idTransport}', 'App\Http\Controllers\DriverController@indexTransport'); 
+
 Route::get('/driver/{id}','App\Http\Controllers\DriverController@show'); 
 Route::post('/driver','App\Http\Controllers\DriverController@store'); 
 Route::post('/driverStatus/{id}','App\Http\Controllers\DriverController@status'); 
