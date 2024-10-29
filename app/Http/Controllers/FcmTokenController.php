0212275a66d22097c62543f4c960f83741051a94
Pachimanok <51484110+Pachimanok@users.noreply.github.com>
2024-10-29 11:10:56 -0300

    modificacion para produccion archivo ok

--- app/Http/Controllers/FcmTokenController.php
+++ app/Http/Controllers/FcmTokenController.php
@@ -48,8 +48,8 @@ class FcmTokenController extends Controller
 
         $file = new Filesystem();
         $archivoPath = storage_path('app/botzero-test-firebase-adminsdk-l750d-5108c493e1.json');
-        return $archivoPath;
         
+
         if ($file->exists($archivoPath)) {
             $archivo = $file->get($archivoPath);
             $config = json_decode($archivo, true);
