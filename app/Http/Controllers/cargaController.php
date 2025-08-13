306e9aee150e73dfcbc2b80be1616e9bf58c2767
pachimanok <priopelliza@gmail.com>
2025-08-13 12:03:56 -0300

    modificaciones para recibir TO de cma

--- app/Http/Controllers/cargaController.php
+++ app/Http/Controllers/cargaController.php
@@ -612,6 +612,7 @@ class cargaController extends Controller
             $carga->ref_customer = $request->input('ref_customer');
             $carga->senasa = $request->input('senasa');
             $carga->senasa_string = $request->input('senasa_string');
+            $carga->cma_t_o = $request->input('cma_t_o');
             $carga->tara = $request->input('tara');
             $carga->tara_string = $request->input('tara_string');
             $carga->referencia_carga = $request->input('referencia_carga');
---
+++ app/Http/Controllers/cmaCgm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class cmaCgm extends Controller
+{
+    // Amrmar para recibir logs en Dashboard. 
+
+    // armar para hacer accion de envío de coordenadas
+
+    // armar para hacer accion de envio de eventos
+
+    
+}
