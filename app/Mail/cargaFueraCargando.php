c16984092fc6d15eeb2b55c3a5628e58d209fddc
pachimanok <priopelliza@gmail.com>
2025-10-06 16:42:22 -0300

    mailable fuera cargando

--- app/Mail/cargaFueraCargando.php
+++ app/Mail/cargaFueraCargando.php
@@ -22,7 +22,9 @@ class cargaFueraCargando extends Mailable
     public function __construct($datos)
     {
         $this->datos = $datos;
-        $this->subject = 'Carga fuera de Zona de Carga. Viaje: '.$datos['cntr'];
+        //$this->subject = 'Carga fuera de Zona de Carga. Viaje: '.$datos['cntr'];
+        $this->subject = 'STATUS // ' . $datos['ref_customer'] . ' - ' . $datos['type'] . ' - ' . $datos['trader'] . ' - 1 * ' . $datos['cntr_type'] . '// BKG: ' . $datos['booking'] . '.';
+
     }
 
     /**
